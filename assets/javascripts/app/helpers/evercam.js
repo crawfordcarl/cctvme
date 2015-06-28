1fa0a9b29f464612d5ebc26ad6e70ac05d316d4f
Christopher McDonald <chrismcband@gmail.com>
2015-06-28 10:30:30 +0100

    Fixed problems with evercam getNearbySnapshots not invoking callback if any snapshots fail. Changed app.getSelfie() to still use nearby camera photos if phone camera is not available.

--- assets/javascripts/app/helpers/evercam.js
+++ assets/javascripts/app/helpers/evercam.js
@@ -1,5 +1,6 @@
 define(function(require, exports, module) {
   var Photo = require('models/photo');
+  var Cameras = require('collections/cameras');
 
   var Evercam = {
     liveCameraUrl: function(camera){
@@ -9,7 +10,7 @@ define(function(require, exports, module) {
         '/live/snapshot' + '?api_id=' + api_id + '&api_key=' + api_secret;
       return url;
     },
-    getSnapshot: function(camera, callback){
+    getSnapshot: function(camera, callback, errorCallback){
       var api_id = app.config.evercam_api_id;
       var api_secret = app.config.evercam_api_secret;
       var url = 'https://api.evercam.io/v1/cameras/' +camera.get('id') +
@@ -24,6 +25,9 @@ define(function(require, exports, module) {
           photo.set('cameraName', camera.get('name'));
           callback(photo);
         }
+      ).fail(function(jqXHR, textStatus){
+          errorCallback(textStatus);
+        }
       );
     },
     getNearbySnapshots: function(location, range, callback){
@@ -31,11 +35,18 @@ define(function(require, exports, module) {
 
       var nearCameras = new Cameras();
       var images = [];
+      var processedCount = 0;
       nearCameras.fetchNearCameras(location, range).then(function(){
         nearCameras.each(function(camera){
           that.getSnapshot(camera, function(photo){
             images.push(photo);
-            if (images.length == nearCameras.length) {
+            processedCount += 1;
+            if (processedCount == nearCameras.length) {
+              callback(images);
+            }
+          }, function(errorMessage){
+            processedCount += 1;
+            if (processedCount == nearCameras.length) {
               callback(images);
             }
           });
